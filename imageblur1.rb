f4598ae443c6bd150c3be140be547286a35bd104
Joe <joeholley@me.com>
2019-10-24 22:23:24 +0000

    backup image blur 1

--- imageblur1.rb
+++ imageblur1.rb
@@ -1,1 +1,21 @@
-puts "hello"
+class Image 
+    attr_accessor :picture
+  
+    def initialize(picture)
+    self.picture = picture
+  end
+
+  def output_image
+    self.picture.each do |data|
+      puts data.join
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
